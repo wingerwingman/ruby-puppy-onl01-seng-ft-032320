226c0c757bdcf1c8e6d1b699d2dd0d7348d37757
Justin Winger <justinwinger4@gmail.com>
2020-03-31 16:44:23 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -12,13 +12,14 @@ class Dog
     @@all
   end
   
-  def self.clear_all
-    @@all = []
-  end
-  
   def self.print_all
     @@all.each do |name|
       puts name
     end
   end
+  
+  def self.clear_all
+    @@all = []
+  end
+  
 end 
